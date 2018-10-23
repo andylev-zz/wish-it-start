93e3436392276dfc394b5922710d665360d58f89
Andy Levis <andylevis@gmail.com>
2018-10-23 13:24:20 +0100

    removed users controllere

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,3 @@ class UsersController < ApplicationController
 #     @user = User.new
 #   end
 
-def new
-  @user = User.new
-end
-
-def index
-   @user = User.new
-end
-
-
-
-def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to users_added_url, :notice => "Thanks we'll be in touch!"
-    else
-      render "new"
-end

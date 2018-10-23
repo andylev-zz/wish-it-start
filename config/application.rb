545671ac5aa63ea679269a7b8f88f1dbf3207291
Andy Levis <andylevis@gmail.com>
2018-10-23 12:33:18 +0100

    yarnupgrade

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,12 @@ module WishItStart
     config.generators do |generate|
           generate.assets false
           generate.helper false
+          config.assets.initialize_on_precompile = false
           generate.test_framework  :test_unit, fixture: false
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

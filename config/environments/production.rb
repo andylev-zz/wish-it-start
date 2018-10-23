8ba0629028f97b730159a9e95a8f5c95e1b45832
Andy Levis <andylevis@gmail.com>
2018-10-23 09:55:09 +0100

    App ready for first deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+config.action_mailer.default_url_options = { :host => 'https://git.heroku.com/wish-it-draft.git' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                      587,
+    domain:                 'https://git.heroku.com/wish-it-draft.git
+',
+    user_name:           ENV["andylevis@gmail.com"],
+    password:              ENV["Vi0letaprilgmail"],
+    authentication:      'plain',
+    enable_starttls_auto: true  }
+
 end
